5dfc13bc53e1cca69646bcd76126e8cb999ee2b2
kharioki <antolisah@gmail.com>
2020-08-22 06:56:22 +0300

    showroom page, appbar and searchbar

---
+++ lib/showroom.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:car_rental/constants.dart';
+import 'package:car_rental/data.dart';
+
+class Showroom extends StatefulWidget {
+  @override
+  _ShowroomState createState() => _ShowroomState();
+}
+
+class _ShowroomState extends State<Showroom> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        brightness: Brightness.dark,
+        title: Text(
+          'Car Rental App',
+          style: GoogleFonts.muli(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        centerTitle: false,
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 16),
+            child: Icon(
+              Icons.menu,
+              color: Colors.black,
+              size: 28,
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            padding: EdgeInsets.only(bottom: 10),
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  hintStyle: TextStyle(fontSize: 16),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(
+                      width: 0,
+                      style: BorderStyle.none,
+                    ),
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey[100],
+                  contentPadding: EdgeInsets.only(left: 30),
+                  suffixIcon: Padding(
+                    padding: EdgeInsets.only(right: 24, left: 16),
+                    child: Icon(
+                      Icons.search,
+                      color: Colors.black,
+                      size: 24,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
