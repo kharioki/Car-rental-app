7e9a33a1ad3d24793bd3156772c17b5de3dd4b64
kharioki <antolisah@gmail.com>
2020-08-22 15:00:51 +0300

    display car images in pageview

--- lib/book_car.dart
+++ lib/book_car.dart
@@ -12,8 +12,186 @@ class BookCar extends StatefulWidget {
 }
 
 class _BookCarState extends State<BookCar> {
+  int _currentImage = 0;
+
+  List<Widget> buildPageIndicator() {
+    List<Widget> list = [];
+    for (var i = 0; i < widget.car.images.length; i++) {
+      list.add(buildIndicator(i == _currentImage));
+    }
+
+    return list;
+  }
+
+  Widget buildIndicator(bool isActive) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 150),
+      margin: EdgeInsets.symmetric(horizontal: 6),
+      height: 8,
+      width: isActive ? 20 : 8,
+      decoration: BoxDecoration(
+        color: isActive ? Colors.black : Colors.grey[400],
+        borderRadius: BorderRadius.all(
+          Radius.circular(12),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.pop(context);
+                              },
+                              child: Container(
+                                width: 45,
+                                height: 45,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(15),
+                                  ),
+                                  border: Border.all(
+                                    color: Colors.grey[300],
+                                    width: 1,
+                                  ),
+                                ),
+                                child: Icon(
+                                  Icons.keyboard_arrow_left,
+                                  color: Colors.black,
+                                  size: 28,
+                                ),
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Container(
+                                  width: 45,
+                                  height: 45,
+                                  decoration: BoxDecoration(
+                                    color: kPrimaryColor,
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(15),
+                                    ),
+                                    border: Border.all(
+                                      color: Colors.grey[300],
+                                      width: 1,
+                                    ),
+                                  ),
+                                  child: Icon(
+                                    Icons.bookmark_border,
+                                    color: Colors.white,
+                                    size: 22,
+                                  ),
+                                ),
+                                SizedBox(width: 16),
+                                Container(
+                                  width: 45,
+                                  height: 45,
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(15),
+                                    ),
+                                    border: Border.all(
+                                      color: Colors.grey[300],
+                                      width: 1,
+                                    ),
+                                  ),
+                                  child: Icon(
+                                    Icons.share,
+                                    color: Colors.black,
+                                    size: 22,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16),
+                        child: Text(
+                          widget.car.model,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 36,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16),
+                        child: Text(
+                          widget.car.brand,
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Expanded(
+                        child: Container(
+                          child: PageView(
+                            physics: BouncingScrollPhysics(),
+                            onPageChanged: (int page) {
+                              setState(() {
+                                _currentImage = page;
+                              });
+                            },
+                            children: widget.car.images.map((path) {
+                              return Container(
+                                padding: EdgeInsets.symmetric(horizontal: 16),
+                                child: Image.asset(
+                                  path,
+                                  fit: BoxFit.scaleDown,
+                                ),
+                              );
+                            }).toList(),
+                          ),
+                        ),
+                      ),
+                      widget.car.images.length > 1
+                          ? Container(
+                              margin: EdgeInsets.symmetric(vertical: 16),
+                              height: 30,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: buildPageIndicator(),
+                              ),
+                            )
+                          : Container(),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
